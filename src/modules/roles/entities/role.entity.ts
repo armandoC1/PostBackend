656afc7cd9e1c035107c9ebc846ba732ab7ebe60
Carlos Hernández <ch148640@gmail.com>
2025-08-21 17:26:30 -0600

    create role entity

--- src/modules/roles/entities/role.entity.ts
+++ src/modules/roles/entities/role.entity.ts
@@ -1,1 +1,27 @@
-export class Role {}
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('roles')
+export class Role {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ type: 'varchar', length: 255 })
+  name: string; //nombre del rol
+
+  @Column({ type: 'text', nullable: true })
+  description: string | null; //descripción del rol
+
+  @CreateDateColumn({ type: 'timestamptz' })
+  created_at: Date; // Fecha/hora de creación
+
+  @UpdateDateColumn({ type: 'timestamptz' })
+  updated_at: Date; // Fecha/hora de actualización
+
+  //Falta la relacion con companiess y permissions
+}
